c23266e4a332f52e5ddbf89a9d2ee306172abbfe
Craig Harman <craig@craigharman.net>
2019-10-31 13:02:38 -0400

    Added project description and URL to setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup
 if __name__ == '__main__':
     setup(
         name="prototurk",
-        version='0.1.0',
-        description="",
+        version='0.1.1',
+        description="Simple server for rapidly prototyping Mechanical Turk interfaces",
 
         packages=[
             'prototurk',
@@ -20,6 +20,7 @@ if __name__ == '__main__':
             'bs4',
         ],
 
+        url="https://github.com/hltcoe/prototurk",
         license="BSD",
         maintainer="Craig Harman",
         maintainer_email="craig@craigharman.net",
