dee00f0cab38552ad06ffb2c85efedf5e44306d6
Craig Harman <craig@craigharman.net>
2019-11-03 10:17:36 -0500

    README now used for package long description

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 ### Added
 - Added CHANGELOG.md file
 - HTTP requests for `.js.map` files now receive 404 response
+- README now used for long description that PyPI displays as "Project description"
 - Task Assignment page now displays Task #
 - Task List page now displays CSV and HTML Template filenames
 
--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+from os import path
 from setuptools import setup
 
 
 if __name__ == '__main__':
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
+
     setup(
-        name="prototurk",
-        version='0.1.1',
-        description="Simple server for rapidly prototyping Mechanical Turk interfaces",
+        name='prototurk',
+        version='0.1.2pre',
+        description='Simple server for rapidly prototyping Mechanical Turk interfaces',
+        long_descripton=long_description,
+        long_descripton_content_type='text/x-rst',
 
         packages=[
             'prototurk',
@@ -20,8 +27,8 @@ if __name__ == '__main__':
             'bs4',
         ],
 
-        url="https://github.com/hltcoe/prototurk",
-        license="BSD",
-        maintainer="Craig Harman",
-        maintainer_email="craig@craigharman.net",
+        url='https://github.com/hltcoe/prototurk',
+        license='BSD',
+        maintainer='Craig Harman',
+        maintainer_email='craig@craigharman.net',
     )
