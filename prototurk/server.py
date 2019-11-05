16c449a18a992a6d18f84cb5aa3fbde7d42663d1
Craig Harman <craig@craigharman.net>
2019-11-04 19:50:30 -0500

    Task now displayed within iFrame.  UI now styled with Bootstrap.

--- prototurk/server.py
+++ prototurk/server.py
@@ -13,7 +13,7 @@ class ProtoTurkServer(object):
     # below.
     CSV_FH = None
     HTML_TEMPLATE = None
-    
+
     def __init__(self, host, port, html_template, csv_file):
         self.host = host
         self.port = port
@@ -24,6 +24,11 @@ class ProtoTurkServer(object):
         bottle.run(host=self.host, port=self.port)
 
 
+def get_template(template_filename):
+    templates_path = os.path.join(os.path.dirname(__file__), 'templates')
+    return open(os.path.join(templates_path, template_filename), 'r', encoding='utf-8').read()
+
+
 @bottle.get('/')
 def index():
     csv_fh = open(ProtoTurkServer.CSV_FILE, 'r', encoding='utf-8')
@@ -31,29 +36,38 @@ def index():
     total_tasks = len(list(reader))
 
     bottle.response.add_header('Cache-Control', 'no-store')
-    
-    templates_path = os.path.join(os.path.dirname(__file__), 'templates')
-    template = open(os.path.join(templates_path, 'task-list.html'), 'r', encoding='utf-8').read()
+
+    template = get_template('task-list.html')
     tpl = bottle.SimpleTemplate(template)
     return tpl.render(
         csv_filename=ProtoTurkServer.CSV_FILE,
         html_template_filename=ProtoTurkServer.HTML_TEMPLATE,
         total_tasks=total_tasks)
 
+
 @bottle.route('/task/<ignore>.js.map')
 def map_js_files(ignore):
     bottle.response.status = 404
 
+
 @bottle.route('/task/<task_id>')
 def task(task_id):
+    template = get_template('task.html')
+
+    tpl = bottle.SimpleTemplate(template)
+    return tpl.render(
+        task_id=task_id)
+
+
+@bottle.route('/task/<task_id>/iframe')
+def task_iframe(task_id):
     csv_fh = open(ProtoTurkServer.CSV_FILE, 'r', encoding='utf-8')
     reader = csv.DictReader(csv_fh)
     tasks = list(reader)
     task_fields = tasks[int(task_id)]
     next_task_id = str((int(task_id) + 1) % len(tasks))
 
-    templates_path = os.path.join(os.path.dirname(__file__), 'templates')
-    template = open(os.path.join(templates_path, 'task-assignment.html'), 'r', encoding='utf-8').read()
+    template = get_template('task-iframe.html')
 
     turk_template = open(ProtoTurkServer.HTML_TEMPLATE, 'r', encoding='utf-8').read()
     soup = BeautifulSoup(turk_template, 'html.parser')
@@ -74,6 +88,7 @@ def task(task_id):
         turk_template_has_submit_button=turk_template_has_submit_button,
         turk_template=turk_template)
 
+
 @bottle.route('/task/<next_task_id>', method='post')
 def task_submit(next_task_id):
     print('FORM DATA:')
@@ -86,6 +101,7 @@ def task_submit(next_task_id):
         except (TypeError, json.decoder.JSONDecodeError):
             print('  %s: %s' % (key, bottle.request.forms[key]))
     bottle.redirect('/task/' + next_task_id)
+
 
 @bottle.route('/static/<filepath:path>')
 def server_static(filepath):
